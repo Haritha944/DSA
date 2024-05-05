3c79f189b8a2e1fe89c6bebdcb04d715328e84ef
Haritha944 <harithams23@gmail.com>
2024-05-05 23:18:56 +0530

    first tree

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# Tree
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.data=value
+        self.right=None
+class Tree:
+    def createNode(self,data):
+        return Node(data)
+    def insert(self,node,data):
+        if node is None:
+            return self.createNode(data)
+        if data<node.data:
+            node.left=self.insert(node.left,data)
+        else:
+            node.right=self.insert(node.right,data)
+        return node
+    def traverse_inorder(self,root):
+        if root is not None:
+            self.traverse_inorder(root.left)
+            print(root.data)
+            self.traverse_inorder(root.right)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tree=Tree()
+root=tree.createNode(5)
+print(root.data)
+tree.insert(root,2)
+tree.insert(root,4)
+tree.insert(root,8)
+tree.insert(root,16)
+tree.insert(root,9)
+print('Inorder------')
+tree.traverse_inorder(root)
